1390e26d293f845c9925a329c83d31d56c137827
shiva prasad <shiva@secondsdigital.com>
2020-05-05 17:39:48 +0530

    Login 2

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login_model extends CI_Model {
+
+class login_model extends CI_Model {
 
     public function __construct()
     {
